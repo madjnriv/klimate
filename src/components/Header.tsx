455b7e3b2a71db29b0c0e16d7f813f108c751687
Daniel Madubuihe <danielmadubuihe@gmail.com>
2025-10-20 01:02:59 +0100

    Fix: updated .gitignore and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const isDark = theme === "dark";
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
-      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+      <div className="container mx-auto flex h-16 items-center gap-x-15 md:justify-between px-4">
         <Link to={"/"}>
           <img
             src={isDark ? "/logo.png" : "/logo2.png"}
